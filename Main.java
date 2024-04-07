9228777db71c123f60487b582191a1fe3965a540
Googles1400 <67911446+Googles1400@users.noreply.github.com>
2024-04-07 11:35:00 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,32 +1,10 @@
-        class Point {
-            private double x;
-            private double y;
-        
-            public Point(double x, double y) {
-                this.x = x;
-                this.y = y;
-            }
-        
-            public double getX() {
-                return x;
-            }
-        
-            public double getY() {
-                return y;
-            }
-        
-            public void setX(double x) {
-                this.x = x;
-            }
-        
-            public void setY(double y) {
-                this.y = y;
-            }
-        }
-        
         public class Main {
             public static void main(String[] args) {
                 Point point = new Point(3.9, 3.1);
-                System.out.println("Współrzędne punktu: (" + point.getX() + ", " + point.getY() + ")");
+
+                point.setX(4.1);
+                point.setY(2.2);
+                
+                System.out.println("Współrzędne punktu: (x:" + point.getX() + ", y:" + point.getY() + ")");
             }
         }
